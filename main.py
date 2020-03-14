547ec34f69e6595ba6f742a2c30ded34fcdc4eed
josh-desktop <rcxwhiz@gmail.com>
2020-03-13 20:21:19 -0600

    Use path.home for making default directory

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import os
 import sys
+from os.path import join
+from pathlib import Path
 
 from Config import cfg
-import InstructorProgram as IP
 
 if cfg.base_directory == 'default':
-    if r'C:/Users/' in sys.argv[0]:
-        s_path = os.path.normpath(sys.argv[0]).split(os.sep)
+    s_path = os.path.normpath(sys.argv[0]).split(os.sep)
 
-        new_base_dir = os.path.join(s_path[0], s_path[1], s_path[2], 'AutoGrader')
-        print(f'[1] Use {new_base_dir} as base directory')
-        print(f'[0] Don\'t use {new_base_dir} as base directory')
-        change_config = IP.tools.input_num_range(0, 1)
-        if change_config == 1:
-            f = open('config.ini', 'r')
-            config_text = f.read().replace('default', new_base_dir)
-            f.close()
-            f = open('config.ini', 'w')
-            f.write(config_text)
-            f.close()
-            print('Please restart')
-            sys.exit()
+    new_base_dir = join(str(Path.home()), 'AutoGrader')
+    print(f'[1] Create {new_base_dir} and use as base directory')
+    print(f'[0] Exit and change config.ini')
+    change_config = -1
+    answer = -1
+    while answer < 0 or answer > 1:
+        try:
+            answer = int(input('\rOption: '))
+        except ValueError:
+            continue
+    if change_config == 1:
+        f = open('config.ini', 'r')
+        config_text = f.read().replace('default', new_base_dir)
+        f.close()
+        f = open('config.ini', 'w')
+        f.write(config_text)
+        f.close()
+        print('Please restart the program')
+        sys.exit()
 
-    print('Please change base directory in config.ini')
     sys.exit()
 
-import InstructorProgram
+import InstructorProgram as IP
 
-InstructorProgram.run()
+IP.run()
