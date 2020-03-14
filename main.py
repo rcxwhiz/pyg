e350ab31e312e81108a50b7c78b325535dd3cf86
josh-desktop <rcxwhiz@gmail.com>
2020-03-13 22:17:48 -0600

    Accounted for if default directory exists

--- main.py
+++ main.py
@@ -3,26 +3,38 @@ from os.path import join
 from pathlib import Path
 
 from Config import cfg
+from InstructorProgram import run
+
+
+def change_config(new_name):
+    f = open('config.ini', 'r')
+    config_text = f.read().replace('default', new_name)
+    f.close()
+    f = open('config.ini', 'w')
+    f.write(config_text)
+    f.close()
+    print('Please restart the program')
+    sys.exit()
+
 
 if cfg.base_directory == 'default':
 
     new_base_dir = join(str(Path.home()), 'AutoGrader')
+    if Path.exists(Path(new_base_dir)):
+        change_config(new_base_dir)
+        run()
+
     print(f'[1] Create {new_base_dir} and use as base directory')
     print(f'[0] Exit and change config.ini')
-    change_config = -1
+    choice = -1
     answer = -1
     while answer < 0 or answer > 1:
         try:
             answer = int(input('\rOption: '))
         except ValueError:
             continue
-    if change_config == 1:
-        f = open('config.ini', 'r')
-        config_text = f.read().replace('default', new_base_dir)
-        f.close()
-        f = open('config.ini', 'w')
-        f.write(config_text)
-        f.close()
+    if choice == 1:
+        change_config(new_base_dir)
         print('Please restart the program')
         sys.exit()
 
