874eeedc7722939768a6804686f7d2332e9534ab
josh <rcxwhiz@gmail.com>
2020-04-08 00:02:22 -0500

    More proper error handling in config.ini

--- Config/cfg.py
+++ Config/cfg.py
@@ -1,14 +1,18 @@
 import configparser
 import os
 import sys
-from pathlib import Path
 from os.path import join
+from pathlib import Path
 
 reader = configparser.ConfigParser()
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-reader.read('config.ini')
+try:
+    reader.read('config.ini')
+except FileNotFoundError:
+    print(f"Couldn't find {join(os.getcwd(), 'config.ini')}")
+    sys.exit()
 
 base_directory = join(str(Path.home()), reader.get('File Structure', 'base_directory'))
 
@@ -17,8 +21,7 @@ max_out_lines = reader.getint('Runtime', 'max_out_lines')
 max_program_time = reader.getint('Runtime', 'max_program_time')
 
 max_threads = min(max_threads, 500)
-if max_threads == 0:
-    max_threads = 500
 if max_threads < 1:
-    print('Max threads in config.ini < 1, exiting...')
-    sys.exit()
+    print('Max threads in config.ini < 1, changing to unlimited')
+if max_threads <= 0:
+    max_threads = 500
